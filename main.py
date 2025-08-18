03dd9a9c8831e9accf84db03ddf9800251739b0f
swoody1101 <swoody1101@gmail.com>
2025-08-18 11:00:41 +0900

    docs: 프로그램 안내문 수정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from .utils import run_as_admin
-from .account_management import rename_admin_account_wmi
+from utils import run_as_admin
+from account_management import rename_admin_account_wmi
 
 __project_name__ = "Windows Security Scanner"
 __version__ = "0.0.0"
@@ -9,7 +9,6 @@ __author__ = "LEE SANG U"
 def show_info():
     run_as_admin()
 
-    """프로젝트 정보를 출력하는 함수"""
     print("---------------------------------")
     print(f"프로젝트명: {__project_name__}")
     print(f"버전: {__version__}")
@@ -19,6 +18,10 @@ def show_info():
     print("계정 관리")
     print("1. Administrator 계정 이름 변경")
     rename_admin_account_wmi()
+
+    print("\n------------------------------------------------")
+    print("모든 보안 점검 및 수정 작업이 완료되었습니다.")
+    input("프로그램을 끝내려면 아무 키나 누르세요...")
 
 
 def main():
