a95fb0c4844872c4d1299d33d6e3755266a39f3f
swoody1101 <swoody1101@gmail.com>
2025-08-18 09:29:54 +0900

    파일 명 변경 및 프로젝트 정보 출력 함수 생성

--- window_security_scan.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Window Security Scan - Version 0.0.0")
-
-if __name__ == "__main__":
-    main()
---
+++ window_security_scanner.py
@@ -0,0 +1,21 @@
+__version__ = "0.0.0"
+__author__ = "LEE SANG U"
+__project_name__ = "Windows Security Scanner"
+
+
+def show_info():
+    """프로젝트 정보를 출력하는 함수"""
+    print("---------------------------------")
+    print(f"프로젝트명: {__project_name__}")
+    print(f"버전: {__version__}")
+    print(f"개발자: {__author__}")
+    print("---------------------------------")
+
+
+def main():
+    show_info()
+    print("프로그램이 시작되었습니다....\n")
+
+
+if __name__ == "__main__":
+    main()
