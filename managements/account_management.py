9407968e3da26c2cfcde8c547714d18299541e79
swoody1101 <swoody1101@gmail.com>
2025-10-27 16:38:58 +0900

    feat: 계정 잠금 기간을 설정하기 위한 set_lockout_duration 기능 생성

--- managements/account_management.py
+++ managements/account_management.py
@@ -94,6 +94,84 @@ def delete_unnecessary_users():
     print("불필요한 계정 삭제를 완료했습니다.\n")
 
 
+# 계정 잠금 임계값 설정
+def set_lockout_threshold():
+    print("계정 잠금 임계값을 5로 설정합니다.")
+    try:
+        subprocess.run(
+            ["net", "accounts", "/lockoutthreshold:5"],
+            check=True,
+            capture_output=True,
+            text=True,
+            encoding="cp949",
+        )
+        print("계정 잠금 임계값이 '5'로 성공적으로 설정되었습니다.\n")
+    except subprocess.CalledProcessError as e:
+        print(f"계정 잠금 임계값 설정 오류: {e.stderr.strip()}")
+        print(
+            "오류 원인: 관리자 권한으로 실행되지 않았거나, 시스템 계정으로 실행할 수 없습니다.\n"
+        )
+    except FileNotFoundError:
+        print("'net' 명령어를 찾을 수 없습니다. 시스템 PATH를 확인해 주세요.\n")
+    except Exception as e:
+        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
+
+
+# 해독 가능한 암호화 설정 비활성화
+def disable_reversible_encryption():
+    print("해독 가능한 암호화 설정 비활성화를 시작합니다.")
+
+    desktop_path = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+    export_cfg_path = os.path.join(desktop_path, "cfg.txt")
+
+    try:
+        print(f"현재 보안 설정을 '{export_cfg_path}' 파일로 내보냅니다.")
+        subprocess.run(
+            ["secedit", "/export", "/cfg", export_cfg_path],
+            check=True,
+            capture_output=True,
+            text=True,
+            encoding="cp949",
+        )
+
+        print("파일에서 'ClearTextPassword' 설정을 '0'으로 변경합니다.")
+        with open(export_cfg_path, "r", encoding="utf-16") as f:
+            lines = f.readlines()
+        found = False
+        with open(export_cfg_path, "w", encoding="utf-8", errors="ignore") as f:
+            for line in lines:
+                if "ClearTextPassword" in line:
+                    f.write("ClearTextPassword = 0\n")
+                    found = True
+                else:
+                    f.write(line)
+            if not found:
+                if not any("[System Access]" in l for l in lines):
+                    f.write("\n[System Access]\n")
+                f.write("ClearTextPassword = 0\n")
+
+        print("수정된 정책 파일을 시스템에 적용합니다.")
+        subprocess.run(
+            ["secedit", "/configure", "/db", "cfg.sdb", "/cfg", export_cfg_path],
+            check=True,
+            capture_output=True,
+            text=True,
+            encoding="cp949",
+        )
+        print("해독 가능한 암호화 설정이 성공적으로 비활성화되었습니다.\n")
+
+    except subprocess.CalledProcessError as e:
+        print(f"정책 적용 실패: {e.stderr.strip()}")
+        print("오류 원인: 관리자 권한으로 실행되었는지 확인하십시오.\n")
+    except FileNotFoundError as e:
+        print(f"파일이 존재하지 않습니다: {e}\n")
+    except Exception as e:
+        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
+
+    finally:
+        cleanup_security_policy_files(desktop_path, export_cfg_path)
+
+
 # 관리자 그룹에 불필요한 계정 권한 회수
 def revoke_unnecessary_admin_privileges():
     admin_list = get_admin_list()
@@ -165,77 +243,6 @@ def revoke_anonymous_everyone_access():
         print(f"예상치 못한 오류 발생: {e}\n")
 
 
-# 계정 잠금 임계값 설정
-def set_lockout_threshold():
-    print("계정 잠금 임계값을 5로 설정합니다.")
-    try:
-        subprocess.run(
-            ["net", "accounts", "/lockoutthreshold:5"],
-            check=True,
-            capture_output=True,
-            text=True,
-            encoding="cp949",
-        )
-        print("계정 잠금 임계값이 '5'로 성공적으로 설정되었습니다.\n")
-    except subprocess.CalledProcessError as e:
-        print(f"계정 잠금 임계값 설정 오류: {e.stderr.strip()}")
-        print(
-            "오류 원인: 관리자 권한으로 실행되지 않았거나, 시스템 계정으로 실행할 수 없습니다.\n"
-        )
-    except FileNotFoundError:
-        print("'net' 명령어를 찾을 수 없습니다. 시스템 PATH를 확인해 주세요.\n")
-    except Exception as e:
-        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
-
-
-# 패스워드 복잡성 설정 활성화
-def enable_password_complexity():
-    print("패스워드 복잡성 설정을 활성화합니다.")
-
-    desktop_path = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
-    export_cfg_path = os.path.join(desktop_path, "cfg.txt")
-
-    try:
-        export_security_settings(export_cfg_path)
-
-        print("파일에서 'PasswordComplexity' 설정을 '1'로 변경합니다.")
-        with open(export_cfg_path, "r", encoding="utf-16") as f:
-            lines = f.readlines()
-        found = False
-        with open(export_cfg_path, "w", encoding="utf-8", errors="ignore") as f:
-            for line in lines:
-                if "PasswordComplexity" in line:
-                    f.write("PasswordComplexity = 1\n")
-                    found = True
-                else:
-                    f.write(line)
-            if not found:
-                if not any("[System Access]" in l for l in lines):
-                    f.write("\n[System Access]\n")
-                f.write("PasswordComplexity = 1\n")
-
-        print("수정된 정책 파일을 시스템에 적용합니다.")
-        subprocess.run(
-            ["secedit", "/configure", "/db", "cfg.sdb", "/cfg", export_cfg_path],
-            check=True,
-            capture_output=True,
-            text=True,
-            encoding="cp949",
-        )
-        print("패스워드 복잡성 설정이 성공적으로 활성화되었습니다.\n")
-
-    except subprocess.CalledProcessError as e:
-        print(f"정책 적용 실패: {e.stderr.strip()}")
-        print("오류 원인: 관리자 권한으로 실행되었는지 확인하십시오.\n")
-    except FileNotFoundError as e:
-        print(f"파일이 존재하지 않습니다: {e}\n")
-    except Exception as e:
-        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
-
-    finally:
-        cleanup_security_policy_files(desktop_path, export_cfg_path)
-
-
 # 암호 사용 기간 제한 없음 비활성화 설정
 def disable_password_never_expires():
     user_list = get_user_list()
@@ -278,38 +285,63 @@ def disable_password_never_expires():
             print(f"'{user}' 계정 처리 중 예기치 않은 오류 발생: {e}\n")
 
 
-# 해독 가능한 암호화 설정 비활성화 설정
-def disable_reversible_encryption():
-    print("해독 가능한 암호화 설정 비활성화를 시작합니다.")
-
-    desktop_path = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
-    export_cfg_path = os.path.join(desktop_path, "cfg.txt")
-
+# 계정 잠금 기간 및 다음 시간 후 계정 잠금 수를 원래대로 설정 60분으로 설정
+# 보안 정책 설정 LockoutBadCount 5, ResetLockoutCount = 60, LockoutDuration = 60
+def set_lockout_duration(
+    lockout_bad_count=5, reset_lockout_count=60, lockout_duration=60
+):
+    print("계정 잠금 기간을 60분으로 설정합니다.")
     try:
-        print(f"현재 보안 설정을 '{export_cfg_path}' 파일로 내보냅니다.")
         subprocess.run(
-            ["secedit", "/export", "/cfg", export_cfg_path],
+            [
+                "net",
+                "accounts",
+                f"/lockoutthreshold:{lockout_bad_count}",
+                f"/lockoutwindow:{reset_lockout_count}",
+                f"/lockoutduration:{lockout_duration}",
+            ],
             check=True,
             capture_output=True,
             text=True,
             encoding="cp949",
         )
+        print("계정 잠금 기간이 '60분'로 성공적으로 설정되었습니다.\n")
+    except subprocess.CalledProcessError as e:
+        print(f"계정 잠금 기간 설정 오류: {e.stderr.strip()}\n")
+        print(
+            "오류 원인: 관리자 권한으로 실행되지 않았거나, 명령에 문제가 있을 수 있습니다.\n"
+        )
+    except FileNotFoundError:
+        print("'net' 명령어를 찾을 수 없습니다. 시스템 PATH를 확인해 주세요.\n")
+    except Exception as e:
+        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
 
-        print("파일에서 'ClearTextPassword' 설정을 '0'으로 변경합니다.")
+
+# 패스워드 복잡성 설정 활성화
+def enable_password_complexity():
+    print("패스워드 복잡성 설정을 활성화합니다.")
+
+    desktop_path = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+    export_cfg_path = os.path.join(desktop_path, "cfg.txt")
+
+    try:
+        export_security_settings(export_cfg_path)
+
+        print("파일에서 'PasswordComplexity' 설정을 '1'로 변경합니다.")
         with open(export_cfg_path, "r", encoding="utf-16") as f:
             lines = f.readlines()
         found = False
         with open(export_cfg_path, "w", encoding="utf-8", errors="ignore") as f:
             for line in lines:
-                if "ClearTextPassword" in line:
-                    f.write("ClearTextPassword = 0\n")
+                if "PasswordComplexity" in line:
+                    f.write("PasswordComplexity = 1\n")
                     found = True
                 else:
                     f.write(line)
             if not found:
                 if not any("[System Access]" in l for l in lines):
                     f.write("\n[System Access]\n")
-                f.write("ClearTextPassword = 0\n")
+                f.write("PasswordComplexity = 1\n")
 
         print("수정된 정책 파일을 시스템에 적용합니다.")
         subprocess.run(
@@ -319,7 +351,7 @@ def disable_reversible_encryption():
             text=True,
             encoding="cp949",
         )
-        print("해독 가능한 암호화 설정이 성공적으로 비활성화되었습니다.\n")
+        print("패스워드 복잡성 설정이 성공적으로 활성화되었습니다.\n")
 
     except subprocess.CalledProcessError as e:
         print(f"정책 적용 실패: {e.stderr.strip()}")
@@ -524,3 +556,48 @@ def restrict_blank_password_logon():
         )
     except Exception as e:
         print(f"예상치 못한 오류 발생: {e}\n")
+
+
+# 원격터미널 접속 가능한 사용자 그룹 제한 설정
+def restrict_rdp_user_group():
+    print("원격터미널 접속 가능한 사용자 그룹 제한 설정을 시작합니다.")
+
+    desktop_path = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+    export_cfg_path = os.path.join(desktop_path, "cfg.txt")
+
+    try:
+        print(f"현재 보안 설정을 '{export_cfg_path}' 파일로 내보냅니다.")
+        export_security_settings(export_cfg_path)
+
+        print("파일에서 'SeRemoteInteractiveLogonRight' 설정을 수정합니다.")
+        with open(export_cfg_path, "r", encoding="utf-16") as f:
+            lines = f.readlines()
+        with open(export_cfg_path, "w", encoding="utf-8", errors="ignore") as f:
+            for line in lines:
+                if "SeRemoteInteractiveLogonRight" in line:
+                    f.write("SeRemoteInteractiveLogonRight = *S-1-5-32-544\n")
+                else:
+                    f.write(line)
+
+        print("수정된 정책 파일을 시스템에 적용합니다.")
+        subprocess.run(
+            ["secedit", "/configure", "/db", "cfg.sdb", "/cfg", export_cfg_path],
+            check=True,
+            capture_output=True,
+            text=True,
+            encoding="cp949",
+        )
+        print(
+            "원격터미널 접속 가능한 사용자 그룹 제한 설정이 성공적으로 수정되었습니다.\n"
+        )
+
+    except subprocess.CalledProcessError as e:
+        print(f"정책 적용 실패: {e.stderr.strip()}")
+        print("오류 원인: 관리자 권한으로 실행되었는지 확인하십시오.\n")
+    except FileNotFoundError as e:
+        print(f"파일이 존재하지 않습니다: {e}\n")
+    except Exception as e:
+        print(f"예기치 않은 오류가 발생했습니다: {e}\n")
+
+    finally:
+        cleanup_security_policy_files(desktop_path, export_cfg_path)
